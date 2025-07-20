783e691ef08b4439bc2c57d74c683a5fc1820db3
hinaarooj1 <hinaarooj978@gmail.com>
2025-07-21 04:10:00 +0500

    fd

--- app/api/auth/session/route.js
+++
@@ -1,8 +0,0 @@
-// app/api/auth/session/route.js
-import { NextResponse } from 'next/server';
-import { getCurrentUser } from '@/lib/auth';
-
-export async function GET(req) {
-  const user = await getCurrentUser(req);
-  return NextResponse.json({ user });
-}
